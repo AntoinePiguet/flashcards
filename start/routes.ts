d44e18fce03521aa6ba60b2b5667104bc78e22fc
Antoine Piguet <pn01kbc@eduvaud.ch>
2025-01-29 16:34:53 +0100

    update(project)
    J'ai ajouté un layout ainsi qu'un template header et l'image avec le lien redirigeant sur la page d'accueil, mais pour ce faire il faut que je configure les rooutes et donc le controller que j'ai aussi créé.

--- start/routes.ts
+++ start/routes.ts
@@ -6,7 +6,10 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
+import DecksController from '#controllers/decks_controller'
 
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
+//router.get('/', [DecksController, 'index']).as('home')
