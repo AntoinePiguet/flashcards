d6d828448db5dfe8c1b17842f6777be59f354e19
Antoine Piguet <pn01kbc@eduvaud.ch>
2025-02-05 14:34:24 +0100

    update(Navigation): bouton accueil et log out redirigent vers les bons urls
    [01:20][DONE]
    J'ai commencé par envoyer un mail à des 3èmes pour avoir accès à leur repos git et donc leurs commits pour savoir dans quel ordre ils avaient fait les choses. l'un deux m'a répondu mais leur projet était totalement différent je n'ai donc pas tenu compte du repo git.
    
    fonctionalités : avec l'aide de ChatGPT, j'ai pu créer des routes fonctionnelles entre mon template de header et ma page home ainsi que ma page login.
    l=> le boutom log out redirige sur la page de login
    => le bouton (logo.png) redirige sur la page d'accueil.

--- start/routes.ts
+++ start/routes.ts
@@ -9,7 +9,20 @@
 import DecksController from '#controllers/decks_controller'
 
 import router from '@adonisjs/core/services/router'
+router.get('/', async ({ response }) => {
+  return response.redirect().toRoute('home')
+})
 
-router.on('/').render('pages/home')
+router
+  .get('/home', async ({ view }) => {
+    return view.render('pages/home')
+  })
+  .as('home')
+
+router
+  .get('/login', async ({ view }) => {
+    return view.render('pages/login')
+  })
+  .as('login')
 
 //router.get('/', [DecksController, 'index']).as('home')
